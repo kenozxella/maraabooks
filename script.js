36d15dab466280b04743a1036ce1788e849c140d
umaraa <ken.ozxella12@gmail.com>
2025-12-25 20:52:33 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
+const menu = document.getElementById("menuPanel");
+
+function toggleMenu() {
+  menu.classList.toggle("open");
+}
+
+// Tema
+if (localStorage.getItem("theme") === "dark") {
+  document.body.classList.add("dark");
+}
+function toggleTheme() {
+  document.body.classList.toggle("dark");
+  localStorage.setItem(
+    "theme",
+    document.body.classList.contains("dark") ? "dark" : "light"
+  );
+}
+
+// Buka genre
 function openGenre(genre) {
-  if (genre === "romance") {
-    window.location.href = "books/romance.html";
-  } else if (genre === "fantasy") {
-    window.location.href = "books/fantasy.html";
-  }
+  if (genre === "novel") window.location.href = "books/novel.html";
+  else if (genre === "pahlawan") window.location.href = "books/pahlawan.html";
+  else if (genre === "sejarah") window.location.href = "books/sejarah.html";
 }
