51cf6511d6602c42909164dd8ddc9ac9cb1ea2f3
umaraa <ken.ozxella12@gmail.com>
2025-12-25 20:25:33 +0700

    Update script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function openGenre(genre) {
+  if (genre === "romance") {
+    window.location.href = "books/romance.html";
+  } else if (genre === "fantasy") {
+    window.location.href = "books/fantasy.html";
+  }
+}
